aaa95753dc355e5bed5ccf458d2ff68538f29508
yuame-21 <yu.ame@northeastern.edu>
2023-11-21 01:15:47 -0600

    fix?

--- modules/routes.js
+++ modules/routes.js
@@ -35,8 +35,9 @@ function ModuleRoutes(app) {
 
     app.get("/api/courses/:cid/modules", (req, res) => {
         const { cid } = req.params;
+        const c = db.courses.find((c) => c.id.$oid === cid);
         const modules = db.modules
-            .filter((m) => m.course === cid);
+            .filter((m) => m.course === c.number);
         res.send(modules);
     });
 }
