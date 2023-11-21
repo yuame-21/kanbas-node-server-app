dd83da9fb5a6e7e9c84cfb525f43afe2a4fc910c
yuame-21 <yu.ame@northeastern.edu>
2023-11-20 19:45:12 -0500

    fix 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express' // import express
 import Hello from "./hello.js"
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import "dotenv/config";
 // import session from "express-session";
